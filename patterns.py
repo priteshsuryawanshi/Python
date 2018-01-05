d0e8038a46679b7ed7661f01d15eae351cad3059
priteshsuryawanshi <34441188+priteshsuryawanshi@users.noreply.github.com>
2018-01-05 15:58:33 +0530

    Add files via upload
    Patterns

---
+++ patterns.py
@@ -0,0 +1,29 @@
+def pattern1():
+	rows = input("Enter number of rows :")
+	for i in range(1,rows+1):
+		s=''
+		for j in range(i):
+			s=s+'*'
+		print s
+		
+def pattern2():
+	rows = input ("ENter number of rows :")
+	for i in range(rows,0,-1):
+		s=''
+		for j in range(i):
+			s=s+'*'
+		print s
+		
+def pattern3():
+	rows = input("Enter number of rows :")
+	for i in range(1,rows+1):
+		s=''
+		for j in range(65,65+i):
+			s=s+chr(j)
+		print s
+		
+def main():
+	pattern3()
+	
+if __name__ == "__main__":
+	main()
